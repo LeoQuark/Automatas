d531314e50457a1a83da7b18ffc0969f9e82630d
Valentina Alarcón <63269085+valentinalarcon@users.noreply.github.com>
2020-06-02 02:59:16 -0400

    semi matriz v.f

--- js/automatas.js
+++ js/automatas.js
@@ -8,10 +8,9 @@ function Quintupla(est_Entrada,arr_alfabeto,est_Iniciales,est_Finales,arrestados
     this.arr_estados = arr_estados
 }
 /*Estados con sus Transiciones*/
-function Estado(nombre,final,lectura,estado_to){
+function Estado(nombre,final,estado_to){
     this.nombre = nombre
     this.final = final // 0 no, 1 si
-    this.lectura = lectura
     this.estado_to = estado_to
 }
 /*-------------------------------------------------------------------------*/
@@ -118,50 +117,97 @@ function crearTablatransicion1(){
 //AUTOMATA FINITO DETERMINISTA
 var entrada=["q1","q2","q3","q4","q5"], alfabeto=["a","b"], inicial= ["q5"], final= ["q2","q3","q4","q5"]
 var estado=[]
-estado[0]= {nombre:"q5",final:["1"] ,lectura: ["a","b"],estado_to:["q4","q3"]}
-estado[1]= {nombre:"q4",final:["1"],lectura:["a","b"],estado_to:["q4","q2"]}
-estado[2]= {nombre:"q3",final:["1"],lectura:["a","b"],estado_to:["q4","q1"]}
-estado[3]= {nombre:"q2",final:["1"],lectura:["a","b"],estado_to:["q4","q1"]}
-estado[4]= {nombre:"q1",final:["0"],lectura:["a","b"],estado_to:[null,"q1"]}
+estado[0]= {nombre:"q5",final:"1" ,estado_to:["q4","q3"]}
+estado[1]= {nombre:"q4",final:"1",estado_to:["q4","q2"]}
+estado[2]= {nombre:"q3",final:"1",estado_to:["q4","q1"]}
+estado[3]= {nombre:"q2",final:"1",estado_to:["q4","q1"]}
+estado[4]= {nombre:"q1",final:"0",estado_to:[null,"q1"]}
 
 var AFDejemplo = {
-    est_Entrada: entrada,
+    est_entrada: entrada,
     arr_alfabeto: alfabeto,
-    est_Iniciales: inicial,
-    est_Finales: final,
+    est_iniciales: inicial,
+    est_finales: final,
     arr_estados : estado,
 }
 console.log("EJEMPLO AFN")
 console.log(AFDejemplo)
 
 /*-----------------------------------------------------*/
-
-function MatrizDiagonal(matriz,indice){
-    console.log(indice)
-    var aux_m = [], aux= 1
-    for (var i=0;i<indice;i++) {
-        for (var j=0;j<aux;j++) { 
-           aux_m[j]=0  //1 
+function arr_estados(matriz1, matriz2,indice,afd){ //
+    //console.log("Funcion arr_estados")  
+    for (let i=0;i<indice-1;i++){
+        matriz1[i]=afd[i]
+    }
+    for (let j=1;j<indice;j++){
+        matriz2[j-1]=afd[j]
+    }
+}
+function buscariEstado(nom_estado,AFDejemplo){
+    //console.log("Funcion buscariEstados") //Busca el indice de un estado en arr_estados de la Quintupla del automata
+    //console.log((AFDejemplo.arr_estados).length)
+    for (let i=0;i<(AFDejemplo.arr_estados).length;i++){
+        if((AFDejemplo.arr_estados)[i].nombre === nom_estado){
+            //console.log(i)
+            return i;
+        }
+    }
+}
+function semiMatriz(AFDejemplo,matriz,estado1,estado2){
+    console.log("Funcion semiMatriz")  
+    var indice = (AFDejemplo.est_entrada).length, aux_m =[],aux=1, e1, e2
+    for (let i=0;i<indice;i++){
+        for (let j=0;j<aux && aux<indice ;j++){ //columna, creo?
+            //console.log("aux",aux)
+            console.log("estado1",estado1[i], "estado2", estado2[j])
+            e1=buscariEstado(estado1[i],AFDejemplo), e2=buscariEstado(estado2[j],AFDejemplo) 
+            console.log("e1,e2 ",e1,e2)
+            
+           if(((AFDejemplo.arr_estados)[e1]).final !== (((AFDejemplo.arr_estados)[e2]).final)){
+            console.log(((AFDejemplo.arr_estados)[e1]).final, " - ", (((AFDejemplo.arr_estados)[e2]).final))
+                aux_m[j]="x";
+                console.log("estados finales diferentes, x")
+           }
+           else{
+                var f1,f2, k1,k2, cont =0
+                //console.log("k ",(AFDejemplo.arr_alfabeto).length )
+                for(let k=0;k<(AFDejemplo.arr_alfabeto).length;k++){
+                    f1=(((AFDejemplo.arr_estados)[e1]).estado_to)[k], f2=(((AFDejemplo.arr_estados)[e2]).estado_to)[k] //f1,f2 estados_to de e1 y e2 
+                    console.log(f1,f2)
+                    if(f1==null || f2==null){
+                        aux_m[j]="x";
+                        console.log("null x")
+                    }
+                    else{
+                        k1=buscariEstado(f1,AFDejemplo), k2=buscariEstado(f2,AFDejemplo)
+                        console.log("k1,k2 ",k1,k2)
+                        if(((AFDejemplo.arr_estados)[k1]).final === "1" && ((AFDejemplo.arr_estados)[k2]).final=== "1" || ((AFDejemplo.arr_estados)[k1]).final === "0" && ((AFDejemplo.arr_estados)[k2]).final=== "0"){
+                            cont++;
+                        }
+                    }
+                }
+                if(cont === (AFDejemplo.arr_alfabeto).length ){
+                    aux_m[j]= estado1[i];
+                }
+                else
+                    aux_m[j]="x"
+           }
         }
         matriz.push(aux_m)
         aux_m = []
-        aux++
+        aux++;
+        
     }
-    return matriz;
 }
-
-
-
 function Simplificar(AFDejemplo){
-    var matriz = []
-    console.log()
-    var indice= AFDejemplo.est_Entrada  //Todos los estados disponibles
-    MatrizDiagonal(matriz,indice.length-1)
+    //console.log("Funcion Simplificar")
+    var matriz = [], estado1 = [], estado2 =[]
+    var ar_estados= AFDejemplo.est_entrada  //Todos los estados disponibles
+    arr_estados(estado1,estado2,ar_estados.length,ar_estados)
+    //MatrizDiagonal(matriz,ar_estados.length-1)
+    //
+    semiMatriz(AFDejemplo,matriz,estado2,estado1)
     console.log(matriz)
-
-
-
-
 }
 Simplificar(AFDejemplo)
 
